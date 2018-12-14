bbb9442b689307b0bea4930cfbd75ab3cc9af6a7
= <wuhao@aegis-data.cn>
2018-12-14 11:51:47 +0800

    :sparkles: 实现了自动推断resultMap，主要针对存在级联查询的情况，可以彻底告别xml文件了

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -11,3 +11,21 @@ CREATE TABLE t_student
 ALTER TABLE t_student
   ADD PRIMARY KEY (id);
 
+CREATE TABLE t_score
+(
+  id         INT AUTO_INCREMENT
+    PRIMARY KEY,
+  score      INT         NOT NULL,
+  student_id VARCHAR(20) NOT NULL,
+  subject_id INT         NOT NULL
+);
+
+CREATE TABLE t_subject
+(
+  id   INT AUTO_INCREMENT
+    PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  CONSTRAINT t_subject_name_uindex
+    UNIQUE (name)
+);
+
