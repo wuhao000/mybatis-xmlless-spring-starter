99dd5a5be7fbbb48967b6b33479c15693a3f9386
= <wuhao@aegis-data.cn>
2019-05-16 20:17:03 +0800

    'fix'

--- src/test/java/com/aegis/SomeTest.kt
+++ src/test/java/com/aegis/SomeTest.kt
@@ -1,10 +1,8 @@
 package com.aegis
 
-import com.aegis.mybatis.bean.Student
 import org.junit.Test
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
-import java.lang.reflect.ParameterizedType
 
 /**
  * Created by 吴昊 on 2018/12/13.
@@ -21,15 +19,7 @@ class SomeTest {
 
   @Test
   fun test2() {
-    Student::class.java.declaredFields.forEach {
-      val type = it.genericType
-      if (type is Class<*>) {
-        println("is class: $type")
-      } else if (type is ParameterizedType) {
-        println(type.rawType is Class<*>)
-        println(type.actualTypeArguments[0] as Class<*>)
-      }
-    }
+
   }
 
 }
